1fba21c8f33554ad057cb04bbbf5886b890b0353
AjaxTalamned <dlyaspamerov@gmail.com>
2011-01-13 06:29:50 -0800

    Key building routines are added.

--- src/main/java/io/redis/rjc/util/KeyUtil.java
+++ src/main/java/io/redis/rjc/util/KeyUtil.java
@@ -23,4 +23,35 @@ public class KeyUtil {
 			throw new RuntimeException(e);
 		}
 	}
+
+    /**
+     * Creates a key from the given blocks using the given separator.
+     * @param separator the separator charachter to use
+     * @param blocks the key's blocks to concatenate
+     * @return the resulting key
+     */
+    public static String toKey(char separator, Object... blocks) {
+        if (blocks.length == 0) {
+            throw new IllegalArgumentException("Empty key");
+        }
+        // concatenate blocks
+        StringBuilder result = new StringBuilder();
+        for (Object block : blocks) {
+            result.append(block);
+            result.append(separator);
+        }
+        // remove trailing separator
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    /**
+     * Creates a key from the given blocks using the default separator character (':').
+     * @param blocks the key's blocks to concatenate
+     * @return the resulting key
+     */
+    public static String toKey(Object... blocks) {
+        return toKey(':', blocks);
+    }
+
 }
