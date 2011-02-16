9d1c575b709d61a92ed2b98bf9d53c56cf08d1a2
e_mzungu <eugene.dolgov@gmail.com>
2011-02-16 23:32:04 +0300

    java doc for Transactions commands

--- src/main/java/org/idevlab/rjc/Session.java
+++ src/main/java/org/idevlab/rjc/Session.java
@@ -25,20 +25,54 @@ public interface Session extends SingleRedisOperations {
 
     String select(int index);
 
+    /**
+     * Marks the start of a transaction block. Subsequent commands will be queued for atomic execution using EXEC.
+     *
+     * @return always OK
+     */
     String multi();
 
+    /**
+     * Flushes all previously queued commands in a transaction and restores the connection state to normal.
+     * <p/>
+     * If WATCH was used, DISCARD unwatches all keys.
+     *
+     * @return always OK
+     */
     String discard();
 
     /**
+     * O(1) for every keys.
+     * <p/>
+     * Marks the given keys to be watched for conditional execution of a transaction.
+     *
+     * @param keys keys to be watched
+     * @return always OK.
      * @since 2.1.0
      */
-    String watch(String... key);
+    String watch(String... keys);
 
     /**
+     * O(1).
+     * <p/>
+     * Flushes all the previously watched keys for a transaction.
+     * <p/>
+     * If you call EXEC or DISCARD, there's no need to manually call UNWATCH.
+     *
+     * @return always OK.
      * @since 2.1.0
      */
     String unwatch();
 
+    /**
+     * Executes all previously queued commands in a transaction and restores the connection state to normal.
+     * <p/>
+     * When using WATCH, EXEC will execute commands only if the watched keys were not modified, allowing for a check-and-set mechanism.
+     *
+     * @return each element being the reply to each of the commands in the atomic transaction.
+     *         <p/>
+     *         When using WATCH, EXEC can return a Null multi-bulk reply if the execution was aborted.
+     */
     List<Object> exec();
 
     void close();
